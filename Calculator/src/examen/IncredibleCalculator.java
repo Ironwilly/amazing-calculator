87e17f47936a859e946fb20e4a7d5144623993db
Guillermo Ferrari Ferrari <claseswilly@gmail.com>
2020-12-22 09:51:11 +0100

    Agregamos funcionalidad de la division decimal

--- Calculator/src/examen/IncredibleCalculator.java
+++ Calculator/src/examen/IncredibleCalculator.java
@@ -24,12 +24,13 @@ public class IncredibleCalculator {
             case 3: // Multiplicar. Debes leer dos números enteros de entrada e imprimir su producto.
                     break;
             case 4: // Multiplicar. Debes leer dos números enteros de entrada e imprimir su división entera.
+            
+                    break;
+            case 5: // Multiplicar. Debes leer dos números enteros de entrada e imprimir su división con decimales.
             	System.out.println("diga el primer número");
             	//ponemos el primer numero a restar;
             	System.out.println("diga el segundo numero");
             	//ponemos el segundo numero a restar
-                    break;
-            case 5: // Multiplicar. Debes leer dos números enteros de entrada e imprimir su división con decimales.
                     break;
 
         }
